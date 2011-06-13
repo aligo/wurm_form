f460a67e60aae0301366a8a37191db5e975d479f
aligo <aligo_x@163.com>
2011-06-13 08:47:43 +0800

    fix

--- lib/wurm_form.rb
+++ lib/wurm_form.rb
@@ -1,12 +1,12 @@
 # WurmForm
 module WurmForm
-  
+
   module ActionController
 
     def self.included(base)
     base.extend(ClassMethods)
     end
-    
+
     module ClassMethods
       def wurm_ajax_validate(form, callback = false)
         define_method(:validate) do
@@ -17,13 +17,9 @@ module WurmForm
             model = form.to_s.gsub('_', '/').camelize.split('::').reduce(Object){|cls, c| cls.const_get(c) }
             #model = Object.const_get(form.to_s.camelize)
             if fields[:id]
-              validity = model.find(fields[:id])
+              validity = model.find(fields[:id]) rescue model.new(fields)
               fields.delete(:id)
-              if validity
-                validity.attributes = fields
-              else
-                validity = model.new(fields)
-              end
+              validity.attributes = fields
             else
               validity = model.new(fields)
             end
@@ -37,7 +33,7 @@ module WurmForm
                 valid[field] = {:valid => true}
               else
                 valid[field] = {:valid => false, :message => validity.errors[field]}
-              end  
+              end
             end
             render :json => valid
           end
@@ -45,5 +41,5 @@ module WurmForm
       end
     end
   end
-  
+
 end
